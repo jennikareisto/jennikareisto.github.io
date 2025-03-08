279c9717fcc82188b73693df5168671b3810bd49
jenni.kareisto@myy.haaga-helia.fi <jenni.kareisto@myy.haaga-helia.fi>
2025-03-08 15:40:43 +0200

    testi

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+// Avaa hampurilaisvalikko
 document.querySelector('.hamburger').addEventListener('click', function() {
-    document.getElementById('mobile-nav').hidden = false;
+    document.getElementById('mobile-nav').style.visibility = 'visible';
 });
 
+// Sulje valikko
 document.querySelector('.close').addEventListener('click', function() {
-    document.getElementById('mobile-nav').hidden = true;
+    document.getElementById('mobile-nav').style.visibility = 'hidden';
 });
