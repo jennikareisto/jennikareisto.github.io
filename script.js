60aadacfaa1fba7caecb736a8968568593a7bd91
jenni.kareisto@myy.haaga-helia.fi <jenni.kareisto@myy.haaga-helia.fi>
2025-03-08 15:11:40 +0200

    testi

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const hamburger = document.querySelector('.hamburger');
-    const mobileNav = document.getElementById('mobile-nav');
-    const closeBtn = document.querySelector('.close');
+document.querySelector('.hamburger').addEventListener('click', function() {
+    document.getElementById('mobile-nav').hidden = false;
+});
 
-    hamburger.addEventListener('click', function() {
-        mobileNav.hidden = false;
-    });
-
-    closeBtn.addEventListener('click', function() {
-        mobileNav.hidden = true;
-    });
+document.querySelector('.close').addEventListener('click', function() {
+    document.getElementById('mobile-nav').hidden = true;
 });
